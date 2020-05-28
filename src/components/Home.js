d4a43794e52786a24a7231cee92a07f24e45ba3e
mbessalle <mbessalle@gmail.com>
2020-05-28 12:25:17 +0200

    home component image and 2 buttons (no func)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 export default function Home() {
-    return (
-        <div>Home</div>
-    )
+  return (
+    <div>
+      <h1>Welcome to Reel Estate</h1>
+      <img src="https://blog.hubspot.com/hubfs/Sales_Blog/real-estate-business-compressor.jpg"></img>
+      <br></br>
+      <button type="button">See listings</button>
+      <br></br>
+      <button type="button">About us</button>
+      <br></br>
+    </div>
+  );
 }
